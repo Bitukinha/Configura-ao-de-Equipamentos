97dc2cf340017f9ae726b48c88380d7472c4727b
Jean Novaes <jeannovaes040@gmail.com>
2026-07-02 00:13:56 -0300

    Test: simple handler response

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
-export default async (req, res) => {
-  const { default: handler } = await import('../dist/server/server.js')
-  const url = new URL(req.url, `http://${req.headers.host}`)
-  const response = await handler.fetch(
-    new Request(url.toString(), {
-      method: req.method,
-      headers: req.headers,
-      body: ['GET', 'HEAD'].includes(req.method) ? undefined : req.body,
-    })
-  )
-  for (const [k, v] of response.headers) res.setHeader(k, v)
-  res.status(response.status)
-  const text = await response.text()
-  res.end(text)
+export default (req, res) => {
+  res.json({ ok: true, url: req.url, time: new Date().toISOString() })
 }
